7c1cec486163be4906b4e6f538962dc0370b3447
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-01-20 03:26:04 +0500

    - created next app - created 'createTodo' and 'getTodo' components and rendered them on UI. * Next start with API

---
+++ app/createTodo/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateTodo() {
+    return (
+        <div>
+            <h2>Create ToDo&rsquo;s</h2>
+            <input type="text"/><br/><br/>
+            <button>Create</button>
+        </div>
+    );
+}
---
+++ app/getTodo/page.tsx
@@ -0,0 +1,8 @@
+export default function GetTodo() {
+    return(
+        <div>
+            <h2>ToDo&rsquo;s</h2>
+            <h4>Todos will display here</h4>
+        </div>
+    );
+}
